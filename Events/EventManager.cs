c38284b78569ed813cb6cd56f698e9cc781fdbc0
Luca Ruiters <lucaruiters@hotmail.nl>
2022-10-24 11:59:35 +0200

    Added a Paramterized event option to the event manager

--- Events/EventManager.cs
+++ Events/EventManager.cs
@@ -8,6 +8,7 @@ namespace Ru1t3rl.Events
     public class EventManager : UnitySingleton<EventManager>
     {
         [SerializeField] private List<CustomEvent> events = new List<CustomEvent>();
+        [SerializeField] private List<CustomEvent<System.EventArgs>> paramEvents = new List<CustomEvent<System.EventArgs>>();
 
         protected override void Awake()
         {
@@ -33,6 +34,19 @@ namespace Ru1t3rl.Events
             events.GetEvent(eventName).AddListener(listener);
         }
 
+        /// <summary>Adds a listenere to an event, if the event doesn't exist it will be created</summary>
+        /// <param name="eventName">The name of the event</param>
+        /// <param name="listener">The listener to add</param>
+        public void AddListener(string eventName, System.Action<System.EventArgs> listener)
+        {
+            if (!paramEvents.ContainsKey(eventName))
+            {
+                paramEvents.Add(new CustomEvent<System.EventArgs>(eventName));
+            }
+
+            paramEvents.GetEvent(eventName).AddListener(listener);
+        }
+
         /// <summary>Removes a listener from an event if the event exists</summary>
         /// <param name="eventName">The name of the event</param>
         /// <param name="listener">The listener to remove</param>
@@ -41,6 +55,17 @@ namespace Ru1t3rl.Events
             if (events.ContainsKey(eventName))
             {
                 events.GetEvent(eventName).RemoveListener(listener);
+            }
+        }
+
+        /// <summary>Removes a listener from an event if the event exists</summary>
+        /// <param name="eventName">The name of the event</param>
+        /// <param name="listener">The listener to remove</param>
+        public void RemoveListener(string eventName, System.Action<System.EventArgs> listener)
+        {
+            if (paramEvents.ContainsKey(eventName))
+            {
+                paramEvents.GetEvent(eventName).RemoveListener(listener);
             }
         }
 
@@ -60,6 +85,22 @@ namespace Ru1t3rl.Events
             }
         }
 
+        /// <summary>Add an event, if the eventName already exists add it as a listener</summary>
+        /// <param name="eventName">The name of the event</param>
+        /// <param name="unityEvent">The unity event to add to the manager</param>
+        public void AddEvent(string eventName, UnityEvent<System.EventArgs> unityEvent)
+        {
+            if (!paramEvents.ContainsKey(eventName))
+            {
+                paramEvents.Add(new CustomEvent<System.EventArgs>(eventName));
+                paramEvents[paramEvents.Count - 1].unityEvent = unityEvent;
+            }
+            else
+            {
+                paramEvents.GetEvent(eventName).AddListener(unityEvent.Invoke);
+            }
+        }
+
         /// <summary>Add an event if it doesn't exist</summary>
         /// <param name="customEvent">The custom event to add to the manager</param>
         public void AddEvent(CustomEvent customEvent)
@@ -67,6 +108,16 @@ namespace Ru1t3rl.Events
             if (!events.Contains(customEvent))
             {
                 events.Add(customEvent);
+            }
+        }
+
+        /// <summary>Add an event if it doesn't exist</summary>
+        /// <param name="customEvent">The custom event to add to the manager</param>
+        public void AddEvent(CustomEvent<System.EventArgs> customEvent)
+        {
+            if (!paramEvents.Contains(customEvent))
+            {
+                paramEvents.Add(customEvent);
             }
         }
 
@@ -90,6 +141,16 @@ namespace Ru1t3rl.Events
             }
         }
 
+        /// <summary>Remove an event if it exists</summary>
+        /// <param name="customEvent">The custom event to remove from the manager</param>
+        public void RemoveEvent(CustomEvent<System.EventArgs> customEvent)
+        {
+            if (paramEvents.Contains(customEvent))
+            {
+                paramEvents.Remove(customEvent);
+            }
+        }
+
         /// <summary>Remove an event if it exists</summary> 
         /// <param name="unityEvent">The unity event to remove from the manager</param>
         public void RemoveEvent(UnityEvent unityEvent)
@@ -100,6 +161,16 @@ namespace Ru1t3rl.Events
             }
         }
 
+        /// <summary>Remove an event if it exists</summary> 
+        /// <param name="unityEvent">The unity event to remove from the manager</param>
+        public void RemoveEvent(UnityEvent<System.EventArgs> unityEvent)
+        {
+            if (paramEvents.ContainsEvent(unityEvent))
+            {
+                paramEvents.Remove(paramEvents.GetEvent(unityEvent));
+            }
+        }
+
         /// <summary>Invoke the event with the given name if it exists</summary>
         /// <param name="eventName">The name of the event to invoke</param>
         public void Invoke(string eventName)
@@ -107,6 +178,16 @@ namespace Ru1t3rl.Events
             if (events.ContainsKey(eventName))
             {
                 events.GetEvent(eventName).Invoke();
+            }
+        }
+
+        /// <summary>Invoke the event with the given name if it exists</summary>
+        /// <param name="eventName">The name of the event to invoke</param>
+        public void Invoke(string eventName, System.EventArgs args)
+        {
+            if (paramEvents.ContainsKey(eventName))
+            {
+                paramEvents.GetEvent(eventName).Invoke(args);
             }
         }
     }
